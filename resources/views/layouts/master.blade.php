0311354e6f813b22e661c99801d8fc762f460174
idstackdev <id.stackdevelopers@gmail.com>
2024-08-11 18:16:28 +0700

    "Refactor code in helper.php, master.blade.php, and menu.blade.php files, including changes to function definitions, script tags, and event listeners."

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -74,7 +74,10 @@
     <script src="{{ asset('assets/js/plugins/chartjs.min.js') }}"></script>
 
     <script src="{{ asset('assets/js/plugins/tilt.min.js') }}"></script>
+
+
     <script>
+        // Loader
         showLoading()
         $(document).ready(function() {
             showLoading(false)
@@ -84,15 +87,15 @@
             const preloader = $(".preloader");
 
             if (show) {
-                preloader.css({
-                    opacity: 1,
-                    visibility: "visible",
-                });
+            preloader.css({
+                opacity: 1,
+                visibility: "visible",
+            });
             } else {
-                preloader.css({
-                    opacity: 0,
-                    visibility: "hidden",
-                });
+            preloader.css({
+                opacity: 0,
+                visibility: "hidden",
+            });
             }
         }
 
@@ -100,257 +103,96 @@
             const button = $(formId).find('button[type="submit"]');
 
             function show() {
-                button
-                    .addClass('btn-load')
-                    .attr("disabled", true)
-                    .html(
-                        `<span class="d-flex align-items-center"><span class"spinner-border flex-shrink-0"></span><span class="flex-grow-1 ms-2"> Loading...</span></span>`
-                    );
+            button
+                .addClass('btn-load')
+                .attr("disabled", true)
+                .html(
+                `<span class="d-flex align-items-center"><span class"spinner-border flex-shrink-0"></span><span
+                class="flex-grow-1 ms-2"> Loading...</span></span>`
+                );
             }
 
             function hide(text = "Save changes") {
-                button.removeClass("btn-load").removeAttr("disabled").text(text);
+            button.removeClass("btn-load").removeAttr("disabled").text(text);
             }
 
             return {
-                show,
-                hide
+            show,
+            hide
             }
         }
-    </script>
 
-    <script>
-        var ctx1 = document.getElementById("chart-line").getContext("2d");
-        var ctx2 = document.getElementById("chart-bar").getContext("2d");
-
-        var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
-
-        gradientStroke1.addColorStop(1, 'rgba(33,82,255,0.1)');
-        gradientStroke1.addColorStop(0.2, 'rgba(33,82,255,0.0)');
-        gradientStroke1.addColorStop(0, 'rgba(33,82,255,0)'); //purple colors
-
-        new Chart(ctx1, {
-            type: "line",
-            data: {
-                labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-                datasets: [{
-                    label: "Tasks",
-                    tension: 0.3,
-                    pointRadius: 2,
-                    pointBackgroundColor: "#2152ff",
-                    borderColor: "#2152ff",
-                    borderWidth: 2,
-                    backgroundColor: gradientStroke1,
-                    data: [40, 45, 42, 41, 40, 43, 40, 42, 39],
-                    maxBarThickness: 6,
-                    fill: true
-                }],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                        },
-                        ticks: {
-                            display: false
-                        }
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                        },
-                        ticks: {
-                            color: '#252f40',
-                            padding: 10
-                        }
-                    },
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: true,
-                            drawTicks: false,
-                            borderDash: [5, 5]
-                        },
-                        ticks: {
-                            display: true,
-                            padding: 10,
-                            color: '#9ca2b7'
-                        }
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: true,
-                            drawOnChartArea: true,
-                            drawTicks: false,
-                            borderDash: [5, 5]
-                        },
-                        ticks: {
-                            display: true,
-                            padding: 10,
-                            color: '#9ca2b7'
-                        }
-                    },
-                },
-            },
-        });
-
-        new Chart(ctx2, {
-            type: "doughnut",
-            data: {
-                labels: ['Done', 'In progress'],
-                datasets: [{
-                    label: "Projects",
-                    weight: 9,
-                    cutout: 50,
-                    tension: 0.9,
-                    pointRadius: 2,
-                    borderWidth: 2,
-                    backgroundColor: ['#2152ff', '#a8b8d8'],
-                    data: [75, 25],
-                    fill: false
-                }],
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                plugins: {
-                    legend: {
-                        display: false,
-                    }
-                },
-                interaction: {
-                    intersect: false,
-                    mode: 'index',
-                },
-                scales: {
-                    y: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                        },
-                        ticks: {
-                            display: false
-                        }
-                    },
-                    x: {
-                        grid: {
-                            drawBorder: false,
-                            display: false,
-                            drawOnChartArea: false,
-                            drawTicks: false,
-                        },
-                        ticks: {
-                            display: false,
-                        }
-                    },
-                },
-            },
-        });
-    </script>
-    <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
-            var options = {
-                damping: '0.5'
-            }
-            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-        }
-    </script>
-
-    <script>
         function handleFormSubmit(selector) {
             let onSuccessCallback;
             let dataTableId;
             let runDefaultSuccessCallback = true;
 
             function init() {
-                $(selector).on('submit', function(e) {
-                    e.preventDefault();
-                    const _form = this;
+            $(selector).on('submit', function(e) {
+                e.preventDefault();
+                const _form = this;
 
-                    $.ajax({
-                        url: _form.action,
-                        method: _form.method,
-                        data: new FormData(_form),
-                        contentType: false,
-                        processData: false,
+                $.ajax({
+                url: _form.action,
+                method: _form.method,
+                data: new FormData(_form),
+                contentType: false,
+                processData: false,
 
-                        beforeSend: function() {
-                            $(_form).find('.is-invalid').removeClass('is-invalid');
-                            $(_form).find('.invalid-feedback').remove();
-                            submitLoader().show();
-                        },
+                beforeSend: function() {
+                    $(_form).find('.is-invalid').removeClass('is-invalid');
+                    $(_form).find('.invalid-feedback').remove();
+                    submitLoader().show();
+                },
 
-                        success: function(res) {
-                            if (runDefaultSuccessCallback) {
-                                $('#modal_action').modal('hide');
-                            }
+                success: function(res) {
+                    if (runDefaultSuccessCallback) {
+                    $('#modal_action').modal('hide');
+                    }
 
-                            if (onSuccessCallback) onSuccessCallback(res);
-                            if (dataTableId) window.LaravelDataTables[dataTableId].ajax.reload();
-                        },
+                    if (onSuccessCallback) onSuccessCallback(res);
+                    if (dataTableId) window.LaravelDataTables[dataTableId].ajax.reload();
+                },
 
-                        complete: function() {
-                            submitLoader().hide();
-                        },
+                complete: function() {
+                    submitLoader().hide();
+                },
 
-                        error: function(err) {
-                            $(_form).find('.is-invalid').removeClass('is-invalid');
-                            $(_form).find('.invalid-feedback').remove();
+                error: function(err) {
+                    $(_form).find('.is-invalid').removeClass('is-invalid');
+                    $(_form).find('.invalid-feedback').remove();
 
-                            const errors = err.responseJSON?.errors;
+                    const errors = err.responseJSON?.errors;
 
-                            if (errors) {
-                                for (let [key, message] of Object.entries(errors)) {
-                                    console.log(key, message);
-                                    const input = $(`[name=${key}]`);
-                                    input.addClass('is-invalid');
-                                    input.parent().append(
-                                        `<div class="invalid-feedback">${message}</div>`);
-                                }
-                            }
-                        }
-                    });
+                    if (errors) {
+                    for (let [key, message] of Object.entries(errors)) {
+                        console.log(key, message);
+                        const input = $(`[name=${key}]`);
+                        input.addClass('is-invalid');
+                        input.parent().append(
+                        `<div class="invalid-feedback">${message}</div>`);
+                    }
+                    }
+                }
                 });
+            });
             }
 
             function onSuccess(cb, runDefault = true) {
-                onSuccessCallback = cb;
-                runDefaultSuccessCallback = runDefault;
-                return this;
+            onSuccessCallback = cb;
+            runDefaultSuccessCallback = runDefault;
+            return this;
             }
 
             function setDataTable(id) {
-                dataTableId = id;
-                return this;
+            dataTableId = id;
+            return this;
             }
 
             return {
-                init,
-                onSuccess,
-                setDataTable,
+            init,
+            onSuccess,
+            setDataTable,
             };
         }
 
@@ -360,59 +202,59 @@
             let runDefaultSuccessCallback = true;
 
             function onSuccess(cb, runDefault = true) {
-                onSuccessCallback = cb;
-                runDefaultSuccessCallback = runDefault;
-                return this;
+            onSuccessCallback = cb;
+            runDefaultSuccessCallback = runDefault;
+            return this;
             }
 
             function onError(cb) {
-                onErrorCallback = cb;
-                return this;
+            onErrorCallback = cb;
+            return this;
             }
 
             function execute() {
-                $.ajax({
-                    url,
-                    method,
+            $.ajax({
+                url,
+                method,
 
-                    beforeSend: function() {
-                        showLoading(true);
-                    },
-                    complete: function() {
-                        showLoading(false);
-                    },
+                beforeSend: function() {
+                showLoading(true);
+                },
+                complete: function() {
+                showLoading(false);
+                },
 
-                    success: function(res) {
-                        if (runDefaultSuccessCallback) {
-                            const modal = $('#modal_action');
-                            modal.html(res);
-                            modal.modal('show');
+                success: function(res) {
+                if (runDefaultSuccessCallback) {
+                    const modal = $('#modal_action');
+                    modal.html(res);
+                    modal.modal('show');
 
-                            // Inisialisasi Choices.js setelah modal ditampilkan
-                            modal.on('shown.bs.modal', function() {
-                                const choiceElements = document.querySelectorAll(
-                                    '.choices-multiple-remove-button');
-                                choiceElements.forEach(element => {
-                                    new Choices(element, {
-                                        removeItemButton: true
-                                    });
-                                });
-                            });
-                        }
+                    // Inisialisasi Choices.js setelah modal ditampilkan
+                    modal.on('shown.bs.modal', function() {
+                    const choiceElements = document.querySelectorAll(
+                        '.choices-multiple-remove-button');
+                    choiceElements.forEach(element => {
+                        new Choices(element, {
+                        removeItemButton: true
+                        });
+                    });
+                    });
+                }
 
-                        if (onSuccessCallback) onSuccessCallback(res);
-                    },
-                    error: function(err) {
-                        if (onErrorCallback) onErrorCallback(err);
-                        console.log(err);
-                    }
-                });
+                if (onSuccessCallback) onSuccessCallback(res);
+                },
+                error: function(err) {
+                if (onErrorCallback) onErrorCallback(err);
+                console.log(err);
+                }
+            });
             }
 
             return {
-                execute,
-                onSuccess,
-                onError
+            execute,
+            onSuccess,
+            onError
             };
         }
     </script>
--- resources/views/page/configuration/menu.blade.php
+++ resources/views/page/configuration/menu.blade.php
@@ -11,83 +11,86 @@
     <div class="row">
         <div class="col-12">
             <div class="card">
-
                 <div class="card-header pb-0">
-                    <div class="d-lg-flex">
+                    <div class="d-lg-flex justify-content-between">
                         <div>
                             <h5 class="mb-0">All Products</h5>
                             <p class="text-sm mb-0">
                                 A lightweight, extendable, dependency-free javascript HTML table plugin.
                             </p>
                         </div>
-                        <div class="ms-auto my-auto mt-lg-0 mt-4">
-                            <div class="ms-auto my-auto">
-                                <a href="{{ route('configuration.menu.create') }}"
-                                    class="btn bg-gradient-primary btn-sm mb-0 add-menu">+&nbsp; New
-                                    Menu</a>
-
-                                <button class="btn btn-outline-primary btn-sm export mb-0 mt-sm-0 mt-1" data-type="csv"
-                                    type="button" name="button">Export</button>
-                            </div>
+                        <div class="ms-auto mt-lg-0 mt-4">
+                            <a href="{{ route('configuration.menu.create') }}"
+                                class="btn bg-gradient-primary btn-sm mb-0 add-menu">+&nbsp; New
+                                Menu</a>
+                            <button class="btn btn-outline-primary btn-sm export mb-0 mt-sm-0 mt-1" data-type="csv"
+                                type="button" name="button">Export</button>
                         </div>
                     </div>
                 </div>
+
                 <div class="card-body px-0 pb-0">
                     <div class="table-responsive p-5">
                         {!! $dataTable->table() !!}
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
+    @push('Script')
+    <!-- jQuery (necessary for DataTables) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- DataTables JS -->
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <!-- Additional Plugin -->
+    <script src="{{ asset('assets/js/plugins/choices.min.js') }}"></script>
+    {!! $dataTable->scripts() !!}
 
-            @push('Script')
-            <!-- jQuery (necessary for DataTables) -->
-            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-            <!-- DataTables JS -->
-            <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-            <script src="{{ asset('assets/js/plugins/choices.min.js') }}"></script>
-            {!! $dataTable->scripts() !!}
-
-            <script>
-                function handleMenuChange() {
-                    // Event listener for level_menu changes
-                    $('[name=level_menu]').on('change', function() {
-                        const wrapper = $('#main_menu_wrapper');
-                        if (this.value === 'sub_menu') {
-                            wrapper.removeClass('d-none');
-                        } else {
-                            wrapper.addClass('d-none');
-                        }
-                    });
+    <script>
+        // Function to handle visibility of main menu based on selected level
+        function handleMenuChange() {
+            const wrapper = $('#main_menu_wrapper');
+            $('[name=level_menu]').on('change', function () {
+                if (this.value === 'sub_menu') {
+                    wrapper.removeClass('d-none');
+                } else {
+                    wrapper.addClass('d-none');
                 }
+            });
+        }
 
-                $(document).ready(function() {
-                    // Initialize menu change handling
+        // Document ready function to initialize event listeners
+        $(document).ready(function () {
+            // Initialize menu change handling
+            handleMenuChange();
+
+            // Event listener for 'add-menu' button click
+            $('.add-menu').on('click', function (e) {
+                e.preventDefault();
+                const url = this.href;
+
+                handleAjax(url).onSuccess(function (res) {
                     handleMenuChange();
+                    handleFormSubmit('#form_action')
+                        .setDataTable('menu-table')
+                        .init();
+                }).execute();
+            });
 
-                    // Event listener for 'add-menu' button click
-                    $('.add-menu').on('click', function(e) {
-                        e.preventDefault();
-                        handleAjax(this.href).onSuccess(function(res) {
-                            handleMenuChange();
-                            handleFormSubmit('#form_action')
-                                .setDataTable('menu-table')
-                                .init();
-                        }).execute();
-                    });
+            // Event listener for actions on the menu table
+            $('#menu-table').on('click', '.action', function (e) {
+                e.preventDefault();
+                const url = this.href;
 
-                    // Event listener for actions on the menu table
-                    $('#menu-table').on('click', '.action', function(e) {
-                        e.preventDefault();
-                        handleAjax(this.href).onSuccess(function(res) {
-                            handleMenuChange();
-                            handleFormSubmit('#form_action')
-                                .setDataTable('menu-table')
-                                .init();
-                        }).execute();
-                    });
-                });
-
-            </script>
-            @endpush
+                handleAjax(url).onSuccess(function (res) {
+                    handleMenuChange();
+                    handleFormSubmit('#form_action')
+                        .setDataTable('menu-table')
+                        .init();
+                }).execute();
+            });
+        });
+    </script>
+    @endpush
 </x-master-layout>
